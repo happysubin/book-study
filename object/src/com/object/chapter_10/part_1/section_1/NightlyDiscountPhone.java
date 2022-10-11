21cfa4a5fd32e2602c12ff5725a4d275c1ae6cb5
happysubin <asb0711@gmail.com>
2022-10-11 16:26:45 +0900

    Feat: Object 10단원 1장까지 학습 완료

---
+++ object/src/com/object/chapter_10/part_1/section_1/NightlyDiscountPhone.java
@@ -0,0 +1,45 @@
+package com.object.chapter_10.part_1.section_1;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 심야 할인 요금제라는 요구사항이 추가됨.
+ * 밤 10시 이후의 통화에 대해 요금을 할인해줌. 이제 일반 요금제와 심야 할인 요금제가 존재.
+ * Phone 코드를 복사해서 클래스를 작성
+ *
+ */
+public class NightlyDiscountPhone {
+
+    private static final int LATE_NIGHT_HOUR = 22;
+
+    private Money nightlyAmount;
+    private Money regularAmount;
+    private Duration seconds;
+    private List<Call> calls = new ArrayList<>();
+
+    public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+        this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
+        this.seconds = seconds;
+    }
+
+    public void call(Call call){
+        calls.add(call);
+    }
+
+    public Money calculateFee(){
+        Money result = Money.ZERO;
+
+        for (Call call : calls) {
+            if(call.getFrom().getHour() >= LATE_NIGHT_HOUR){
+                result = result.plus(nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            }
+            else{
+                result = result.plus(regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            }
+        }
+        return result;
+    }
+}
---
+++ object/src/com/object/chapter_10/part_1/section_2/NightlyDiscountPhone.java
@@ -0,0 +1,44 @@
+package com.object.chapter_10.part_1.section_2;
+
+import com.object.chapter_10.part_1.section_1.Call;
+import com.object.chapter_10.part_1.section_1.Money;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NightlyDiscountPhone {
+
+    private static final int LATE_NIGHT_HOUR = 22;
+
+    private Money nightlyAmount;
+    private Money regularAmount;
+    private Duration seconds;
+    private List<Call> calls = new ArrayList<>();
+    private double taxRate;
+
+    public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds, double taxRate) {
+        this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
+        this.seconds = seconds;
+        this.taxRate = taxRate;
+    }
+
+    public void call(Call call){
+        calls.add(call);
+    }
+
+    public Money calculateFee(){
+        Money result = Money.ZERO;
+
+        for (Call call : calls) {
+            if(call.getFrom().getHour() >= LATE_NIGHT_HOUR){
+                result = result.plus(nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            }
+            else{
+                result = result.plus(regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            }
+        }
+        return result.minus(result.times(taxRate));  //만약 여기 minus가 나타난다면 오류 발생!!! 중복 코드는 이런 오류를 놓치게 만든다. 너무 많은 코드를 똑같이 수정하므스
+    }
+}
