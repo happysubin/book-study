d661b67ccf8567cb81af63a43ad78988b66e30a7
happysubin <asb0711@gmail.com>
2022-10-12 00:20:25 +0900

    Feat: Object 11단원 2장까지 학습

---
+++ object/src/com/object/chapter_11/part_2/RateDiscountNightlyDiscountPhone.java
@@ -0,0 +1,22 @@
+package com.object.chapter_11.part_2;
+
+import com.object.chapter_10.part_1.section_1.Call;
+import com.object.chapter_10.part_1.section_1.Money;
+
+import java.time.Duration;
+
+
+public class RateDiscountNightlyDiscountPhone extends NightlyDiscountPhone {
+
+    private Money discountAmount;
+
+    public RateDiscountNightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds, Money discountAmount) {
+        super(nightlyAmount, regularAmount, seconds);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money afterCalculated(Money fee) {
+        return fee.minus(discountAmount);
+    }
+}
---
+++ object/src/com/object/chapter_11/part_2/TaxableNightlyDiscountPhone.java
@@ -0,0 +1,22 @@
+package com.object.chapter_11.part_2;
+
+import com.object.chapter_10.part_1.section_1.Call;
+import com.object.chapter_10.part_1.section_1.Money;
+
+import java.time.Duration;
+
+
+public class TaxableNightlyDiscountPhone extends NightlyDiscountPhone {
+
+    private double taxRate;
+
+    public TaxableNightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds, double taxRate) {
+        super(nightlyAmount, regularAmount, seconds);
+        this.taxRate = taxRate;
+    }
+
+    @Override
+    protected Money afterCalculated(Money fee) {
+        return fee.plus(fee.times(taxRate));
+    }
+}
