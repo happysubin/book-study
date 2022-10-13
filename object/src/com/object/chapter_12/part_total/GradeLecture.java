87105a2e178c9cae4984296246995593a21e4e67
happysubin <asb0711@gmail.com>
2022-10-13 14:21:25 +0900

    Feat: Object 12단원 4장까지 학습

---
+++ object/src/com/object/chapter_12/part_total/GradeLecture.java
@@ -0,0 +1,37 @@
+package com.object.chapter_12.part_total;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.joining;
+
+public class GradeLecture extends Lecture{
+
+    private List<Grade> grades;
+
+    public GradeLecture(int pass, String title, List<Integer> scores, List<Grade> grades) {
+        super(pass, title, scores);
+        this.grades = grades;
+    }
+
+    @Override
+    public String evaluate() {
+        return super.evaluate() +", " + gradesStatistics();
+    }
+
+    private String gradesStatistics() {
+        return grades.stream()
+                .map(grade -> format(grade))
+                .collect(joining(" "));
+    }
+
+    private String format(Grade grade){
+        return String.format("%s:$d", grade.getName(), gradeCount(grade));
+    }
+
+    private long gradeCount(Grade grade){
+        return getScores().stream()
+                .filter(grade::include)
+                .count();
+    }
+
+}
