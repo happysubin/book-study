8eb6cb65d3c2558b9c5aae6d92ce241ba7806b9a
happysubin <asb0711@gmail.com>
2022-12-08 15:50:01 +0900

    Feat: 9장 중간 점검 준비

--- 자바 Next Step 웹프로그래밍/jwp-basic-step-self-check/src/main/java/core/mvc/JsonView.java
+++ 자바 Next Step 웹프로그래밍/jwp-basic-step-self-check/src/main/java/core/mvc/JsonView.java
@@ -1,20 +1,22 @@
-package core.mvc.section_8_3;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+package core.mvc;
 
 import java.io.PrintWriter;
 import java.util.Map;
 
-public class JsonView implements View{
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonView implements View {
 
     @Override
-    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         response.setContentType("application/json;charset=UTF-8");
         PrintWriter out = response.getWriter();
         out.print(mapper.writeValueAsString(model));
     }
+
 }
