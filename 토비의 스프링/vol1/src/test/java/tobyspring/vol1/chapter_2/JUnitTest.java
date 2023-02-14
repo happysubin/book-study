5abe3424893ad926ae11a8f0f01f30858e0af90c
happysubin <asb0711@gmail.com>
2023-02-14 16:36:06 +0900

    Feat: 2장 Junit 학습테스트

---
+++ 토비의 스프링/vol1/src/test/java/tobyspring/vol1/chapter_2/JUnitTest.java
@@ -0,0 +1,29 @@
+package tobyspring.vol1.chapter_2;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class JUnitTest {
+
+    static JUnitTest test;
+
+    @Test
+    void test1(){
+        assertThat(this).isNotSameAs(test);
+        test = this;
+    }
+
+    @Test
+    void test2(){
+        assertThat(this).isNotSameAs(test);
+        test = this;
+    }
+
+    @Test
+    void test3(){
+        assertThat(this).isNotSameAs(test);
+        test = this;
+    }
+}
